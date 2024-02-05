1fe0de2ab2117a71aab6a4fc612ce4900e7ddfc3
rob kerr <reallyrobkerr@gmail.com>
2024-02-05 14:52:41 -0800

    update imports and exports

--- sick-fits/frontend/components/index.js
+++ sick-fits/frontend/components/index.js
@@ -1,3 +1,5 @@
 export { default as Nav } from './Nav.js';
 export { default as Page } from './Page.js';
 export { default as Header } from './Header.js';
+export { default as Products } from './Products.js';
+export { default as CreateProduct } from './CreateProduct.js';
