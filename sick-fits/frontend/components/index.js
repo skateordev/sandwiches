947d80c5141250c7aee3b7e4f71faf0d8795ffeb
rob kerr <reallyrobkerr@gmail.com>
2024-02-06 16:16:32 -0800

    Update naming, exports; housekeeping 🧽

--- sick-fits/frontend/components/index.js
+++ sick-fits/frontend/components/index.js
@@ -2,4 +2,5 @@ export { default as Nav } from './Nav.js';
 export { default as Page } from './Page.js';
 export { default as Header } from './Header.js';
 export { default as Products } from './Products.js';
+export { default as ErrorMessage } from './ErrorMessage.js';
 export { default as CreateProduct } from './CreateProduct.js';
