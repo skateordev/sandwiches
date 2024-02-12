201500de4e4c27204c8c8dfd6a3cc43b4c535296
rob kerr <reallyrobkerr@gmail.com>
2024-02-11 18:47:12 -0800

    paginate all products query

--- sick-fits/frontend/components/Products/queries/allProductsQuery.js
+++ sick-fits/frontend/components/Products/queries/allProductsQuery.js
@@ -1,8 +1,14 @@
 import gql from 'graphql-tag';
 
 const ALL_PRODUCTS_QUERY = gql`
-  query ALL_PRODUCTS_QUERY {
-    allProducts {
+  query ALL_PRODUCTS_QUERY(
+    $skip: Int = 0, # represents how many products per page aka how many to "skip" when paging
+    $first: Int # what defines the first "batch"
+  ) {
+    allProducts(
+      skip: $skip
+      first: $first
+    ) {
       id
       name
       photo {
