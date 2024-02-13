e3675a506aaddf014074a505855f178a0b2e278a
rob kerr <reallyrobkerr@gmail.com>
2024-02-12 16:51:08 -0800

    add signin to nav

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,13 +1,21 @@
 import Link from 'next/link';
 import { NavStyled } from './styles';
+import useUser from './User/User';
 
 export default function Nav() {
+  const user = useUser();
+
   return (
     <NavStyled>
       <Link href="/products">Products</Link>
-      <Link href="/sell">Sell</Link>
-      <Link href="/orders">Orders</Link>
-      <Link href="/account">Account</Link>
+      {user && (
+        <>
+          <Link href="/sell">Sell</Link>
+          <Link href="/orders">Orders</Link>
+          <Link href="/account">Account</Link>
+        </>
+      )}
+      {!user && <Link href="/signin">Sing In 🎤</Link>}
     </NavStyled>
   );
 }
