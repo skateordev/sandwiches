efbef273e19a2fbf714b1ea0c65af48e9663d558
rob kerr <reallyrobkerr@gmail.com>
2024-02-14 01:33:06 -0800

    rename password request component and mutation

--- sick-fits/frontend/components/index.js
+++ sick-fits/frontend/components/index.js
@@ -8,6 +8,6 @@ export { default as Products } from './Products/Products';
 export { default as Pagination } from './Pagination/Pagination';
 export { default as ErrorMessage } from './ErrorMessage';
 export { default as CreateProduct } from './CreateProduct/CreateProduct';
+export { default as DeleteProduct } from './DeleteProduct/DeleteProduct';
 export { default as SingleProduct } from './SingleProduct/SingleProduct';
 export { default as UpdateProduct } from './UpdateProduct/UpdateProduct';
-export { default as DeleteProduct } from './DeleteProduct/DeleteProduct';
