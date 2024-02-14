1b1fb2b9941513a309eca63217258be144719fdf
rob kerr <reallyrobkerr@gmail.com>
2024-02-13 19:01:36 -0800

    show SignUp on signin page

--- sick-fits/frontend/pages/signin.js
+++ sick-fits/frontend/pages/signin.js
@@ -1,7 +1,17 @@
-import { SignIn } from '../components';
+import styled from 'styled-components';
+import { SignIn, SignUp } from '../components';
+
+const GridDiv = styled.div`
+  display: grid;
+  grid-gap: 1rem;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+`;
 
 export default function SignInPage() {
   return (
-    <SignIn />
+    <GridDiv>
+      <SignIn />
+      <SignUp />
+    </GridDiv>
   );
 }
