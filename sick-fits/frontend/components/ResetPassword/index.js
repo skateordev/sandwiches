23db963821ed86060a375eb7efb17ff31ef0e638
rob kerr <reallyrobkerr@gmail.com>
2024-02-14 01:34:18 -0800

    exports

---
+++ sick-fits/frontend/components/ResetPassword/index.js
@@ -0,0 +1,2 @@
+export { default as ResetPassword } from './ResetPassword';
+export { default as RequestPasswordReset } from './RequestPasswordReset';
---
+++ sick-fits/frontend/components/ResetPassword/mutations/index.js
@@ -0,0 +1,2 @@
+export { default as RESET_PASSWORD_MUTATION } from './resetPasswordMutation';
+export { default as REQUEST_PASSWORD_RESET_MUTATION } from './requestPasswordResetMutation';
