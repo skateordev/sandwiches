295d8f213804a7fefb212f253cdad52c126ae8e4
rob kerr <reallyrobkerr@gmail.com>
2024-02-13 19:01:23 -0800

    export SignUp

--- sick-fits/frontend/components/index.js
+++ sick-fits/frontend/components/index.js
@@ -2,6 +2,7 @@ export { default as Nav } from './Nav';
 export { default as Page } from './Page';
 export { default as Header } from './Header';
 export { default as SignIn } from './SignIn/SignIn';
+export { default as SignUp } from './SignUp/SignUp';
 export { default as SignOut } from './SignOut/SignOut';
 export { default as Products } from './Products/Products';
 export { default as Pagination } from './Pagination/Pagination';
