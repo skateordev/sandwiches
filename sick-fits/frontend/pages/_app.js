eed6411257f45b534d8e1912eb9106808d1188d6
rob kerr <reallyrobkerr@gmail.com>
2024-02-14 23:09:51 -0800

    add CartStateProvider to _app

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -5,6 +5,7 @@ import { ApolloProvider } from '@apollo/client';
 import { Page } from '../components';
 import withData from '../lib/withData';
 import '../components/styles/nprogress.css';
+import { CartStateProvider } from '../lib/cartState';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeError', () => NProgress.done());
@@ -13,10 +14,12 @@ Router.events.on('routeChangeComplete', () => NProgress.done());
 function App({ Component, pageProps, apollo }) {
   return (
     <ApolloProvider client={apollo}>
-      <Page>
-        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-        <Component {...pageProps} />
-      </Page>
+      <CartStateProvider>
+        <Page>
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          <Component {...pageProps} />
+        </Page>
+      </CartStateProvider>
     </ApolloProvider>
   );
 }
