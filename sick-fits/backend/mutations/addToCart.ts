cf72dbae7899e2dd0a669bc77f981407034c04e0
rob kerr <reallyrobkerr@gmail.com>
2024-02-17 22:32:32 -0800

    fix addToCart mutation

--- sick-fits/backend/mutations/addToCart.ts
+++ sick-fits/backend/mutations/addToCart.ts
@@ -8,11 +8,13 @@ async function addToCart(
   context: KeystoneContext
 ): Promise<CartItemCreateInput> {
   console.log('¡Adding into cart!');
+
   // 1. Query the current user -- are the signed in?
   const sesh = context.session as Session;
   if (!sesh.itemId) {
     throw new Error('You gotta log in to do that!');
   }
+
   // 2. Query the current user's cart
   const allCartItems = await context.lists.CartItem.findMany({
     where: { user: { id: sesh.itemId }, product: { id: productId } },
@@ -21,23 +23,30 @@ async function addToCart(
 
   // destructure first item from allCartItems into existingCartItem
   const [existingCartItem] = allCartItems;
-/* eslint-disable */
+
   // 3. See if the current item is in their cart
+  /* eslint-disable */
   if (existingCartItem) {
+    console.log({ existingCartItem });
     console.log(`There's already ${existingCartItem.quantity}, adding 1 moar!`);
     // 4a. If it is, increment by 1 (or number specified, if we do that)
 
     return context.lists.CartItem.updateOne({
       id: existingCartItem.id,
       data: { quantity: existingCartItem.quantity + 1 },
+      resolveFields: false,
     });
   }
 
   // 4b. If it isn't, create a new cart item
   return context.lists.CartItem.createOne({
-    user: { connect: { id: sesh.itemId } },
-    product: { connect: { id: productId } },
+    data: {
+      user: { connect: { id: sesh.itemId } },
+      product: { connect: { id: productId } },
+    },
+    resolveFields: false,
   });
+  /* eslint-enable */
 }
 
 export default addToCart;
