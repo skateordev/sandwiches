85ef4f77c91c1788dc9c8f2e5d13c6f98421e8e3
rob kerr <reallyrobkerr@gmail.com>
2024-02-17 22:32:54 -0800

    noobify comments a bit

--- sick-fits/backend/mutations/index.ts
+++ sick-fits/backend/mutations/index.ts
@@ -1,15 +1,19 @@
 import { graphQLSchemaExtension } from '@keystone-next/keystone/schema';
 import addToCart from './addToCart';
 
-// fake some syntax highlighting
+// fake some syntax highlighting for inside template literal
 const graphql = String.raw;
 
 export const extendGraphqlSchema = graphQLSchemaExtension({
+  /* what is the name of the method, what does it take in
+  and what does it return */
   typeDefs: graphql`
     type Mutation {
       addToCart(productId: ID): CartItem
     }
   `,
+  /* resolvers are links to nodejs functions that
+     will run when requested upon via graphql api */
   resolvers: {
     Mutation: {
       addToCart,
