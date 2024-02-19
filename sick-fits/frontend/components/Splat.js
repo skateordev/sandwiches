ec685e90fefec072dc3a58f1c3199698fc713dea
rob kerr <reallyrobkerr@gmail.com>
2024-02-18 23:47:37 -0800

    Animate Cart Splat when adding items to cart

--- sick-fits/frontend/components/Splat.js
+++ sick-fits/frontend/components/Splat.js
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import styled from 'styled-components';
 
 const SplatStyled = styled.div`
@@ -15,9 +16,55 @@ const SplatStyled = styled.div`
   box-shadow: 0.25rem -0.15rem 0px var(--black);
 `;
 
+const AnimationStyled = styled.span`
+  position: relative;
+
+  .count {
+    display: block;
+    position: relative;
+    transition: transform 666ms;
+    backface-visibility: hidden;
+  }
+
+  .count-enter {
+    transform: scale(4) rotateX(0.5turn);
+  }
+
+  .count-enter-active {
+    transform: rotateX(0);
+    background: chartreuse;
+  }
+
+  .count-exit {
+    top: 0;
+    position: absolute;
+    transform: rotateX(0);
+  }
+
+  .count-exit-active {
+    background: #f1f;
+    transform: scale(4) rotateX(0.5turn);
+  }
+`;
+
 export function Splat({ count }) {
   return (
-    <SplatStyled className="splat">{count}</SplatStyled>
+    <AnimationStyled>
+      <TransitionGroup>
+        <CSSTransition
+          key={count}
+          timeout={{
+            enter: 666,
+            exit: 666,
+          }}
+          className="count"
+          classNames="count"
+          unmountOnExit
+        >
+          <SplatStyled className="splat">{count}</SplatStyled>
+        </CSSTransition>
+      </TransitionGroup>
+    </AnimationStyled>
   );
 }
 
