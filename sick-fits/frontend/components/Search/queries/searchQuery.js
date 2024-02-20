f5c57aca3481c8bc574521b33eda1b98bc7de2a2
rob kerr <reallyrobkerr@gmail.com>
2024-02-19 23:59:51 -0800

    create searchQuery

---
+++ sick-fits/frontend/components/Search/queries/searchQuery.js
@@ -0,0 +1,24 @@
+import gql from 'graphql-tag';
+
+const SEARCH_PRODUCTS_QUERY = gql`
+  query SEARCH_PRODUCTS_QUERY($searchTerm: String!) {
+    searchTerms: allProducts(
+      where: {
+        OR: [
+          { name_contains_i: $searchTerm }
+          { description_contains_i: $searchTerm }
+        ]
+      }
+    ) {
+      id
+      name
+      photo {
+        image {
+          publicUrlTransformed
+        }
+      }
+    }
+  }
+`;
+
+export default SEARCH_PRODUCTS_QUERY;
