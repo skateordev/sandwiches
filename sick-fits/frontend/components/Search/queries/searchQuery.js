0e6ce3a9ee8455afd0c3f73b0dd46848e378de81
rob kerr <reallyrobkerr@gmail.com>
2024-02-20 01:13:20 -0800

    naming is hard

--- sick-fits/frontend/components/Search/queries/searchQuery.js
+++ sick-fits/frontend/components/Search/queries/searchQuery.js
@@ -2,7 +2,7 @@ import gql from 'graphql-tag';
 
 const SEARCH_PRODUCTS_QUERY = gql`
   query SEARCH_PRODUCTS_QUERY($searchTerm: String!) {
-    searchTerms: allProducts(
+    searchResults: allProducts(
       where: {
         OR: [
           { name_contains_i: $searchTerm }
