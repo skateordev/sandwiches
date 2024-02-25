3f1b45256fe62b9ee88702bb5c5fbffafcb2237a
rob kerr <reallyrobkerr@gmail.com>
2024-02-25 02:13:00 -0800

    hookup order checkout/stripe payment on the front end

--- sick-fits/frontend/components/Checkout/Checkout.js
+++ sick-fits/frontend/components/Checkout/Checkout.js
@@ -8,7 +8,9 @@ import { loadStripe } from '@stripe/stripe-js';
 import styled from 'styled-components';
 import { useState } from 'react';
 import nProgress from 'nprogress';
+import { useMutation } from '@apollo/client';
 import { SickButton } from '../styles';
+import CREATE_ORDER_MUTATION from './mutations/createOrderMutation';
 
 const CheckoutFormStyled = styled.form`
   box-shadow: 0 1px 2px 2px rgba(0,0,0,0.04);
@@ -22,33 +24,48 @@ const CheckoutFormStyled = styled.form`
 const stripeLib = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY);
 
 function CheckoutForm() {
-  const [error, setError] = useState();
-  const [isLoading, setIsLoading] = useState(false);
-
   const stripe = useStripe();
   const elements = useElements();
+  const [error, setError] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [checkout, { error: graphQLError }] = useMutation(CREATE_ORDER_MUTATION);
 
   const handleSubmit = async (evt) => {
     // 1. stop the form from submitting and turn the loader on
     evt.preventDefault();
+    setIsLoading(true);
 
     // 2. start the page transition
     nProgress.start();
 
     // 3. create the payment method via stripe (toked comes back here if successful)
-    const { error, paymentMethod } = await stripe.createPaymentMethod({
+    const { error: stripeError, paymentMethod } = await stripe.createPaymentMethod({
       type: 'card',
       card: elements.getElement(CardElement),
     });
+    console.log(paymentMethod);
 
-    // 4. handle any error afrom stripe
-    if (error) {
-      setError(error);
+    // 4. handle any error from stripe
+    if (stripeError) {
+      setError(stripeError);
+      nProgress.done();
+      return; // stop the checkout from happening
     }
 
     // 5. send the token from step 3 to our keystone server, via a custom mutation!
+    const order = await checkout({
+      variables: {
+        token: paymentMethod.id,
+      },
+    });
+
+    console.log('Order cumplete💦');
+    console.log(order);
+
     // 6. chage the page to view the order
+
     // 7. close the cart
+
     // 8. turn the loader off
     setIsLoading(false);
     nProgress.done();
@@ -57,6 +74,7 @@ function CheckoutForm() {
   return (
     <CheckoutFormStyled onSubmit={handleSubmit}>
       {error && <p style={{ fontSize: '0.75rem' }}>{error.message}</p>}
+      {error && <p style={{ fontSize: '0.75rem' }}>{graphQLError.message}</p>}
       <CardElement />
       <SickButton>TAKE MY MONEY</SickButton>
     </CheckoutFormStyled>
