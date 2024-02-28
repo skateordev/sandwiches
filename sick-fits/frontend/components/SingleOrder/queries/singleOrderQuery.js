b3cce60975d9f64c55a60d7adcc5b8965c83f086
rob kerr <reallyrobkerr@gmail.com>
2024-02-27 18:54:47 -0800

    create single order query

---
+++ sick-fits/frontend/components/SingleOrder/queries/singleOrderQuery.js
@@ -0,0 +1,27 @@
+import gql from 'graphql-tag';
+
+const SINGLE_ORDER_QUERY = gql`
+  query SINGLE_ORDER_QUERY($id: ID!) {
+    Order(where: {
+      id: $id
+    }) {
+      id
+      total
+      items {
+        id
+        name
+        price
+        quantity
+        description
+        photo {
+          altText
+          image {
+            publicUrlTransformed
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default SINGLE_ORDER_QUERY;
