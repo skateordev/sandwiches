c0a7276ab69a2e617b966873306686a461dfe98f
rob kerr <reallyrobkerr@gmail.com>
2024-02-28 23:59:56 -0800

    centre the Pagination on orders page

--- sick-fits/frontend/pages/orders/index.js
+++ sick-fits/frontend/pages/orders/index.js
@@ -1,15 +1,25 @@
 import { useRouter } from 'next/router';
+import styled from 'styled-components';
 import { Orders, Pagination } from '../../components';
+
+const OrdersPageStylee = styled.div`
+  display: grid;
+  align-items: center;
+
+  > div {
+    margin: 16px auto;
+  }
+`;
 
 export default function OrdersPage() {
   const { query } = useRouter();
   const currentPage = parseInt(query.paged, 10);
 
   return (
-    <div>
+    <OrdersPageStylee>
       <Pagination currentPage={currentPage || 1} />
       <Orders currentPage={currentPage || 1} />
       <Pagination currentPage={currentPage || 1} />
-    </div>
+    </OrdersPageStylee>
   );
 }
