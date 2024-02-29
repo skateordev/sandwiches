62aa07e81326f8e1b35e21524c9e8a15645d5ef3
rob kerr <reallyrobkerr@gmail.com>
2024-02-29 01:54:26 -0800

    naming is hard

--- sick-fits/frontend/components/constants.js
+++ sick-fits/frontend/components/constants.js
@@ -1,9 +1,9 @@
 const ITEMS_PER_PAGE = 4;
-const ITEM_GROUP_ORDERS = 'orders';
-const ITEM_GROUP_PRODUCTS = 'products';
+const ITEM_TYPE_ORDERS = 'orders';
+const ITEM_TYPE_PRODUCTS = 'products';
 
 export {
   ITEMS_PER_PAGE,
-  ITEM_GROUP_ORDERS,
-  ITEM_GROUP_PRODUCTS,
+  ITEM_TYPE_ORDERS,
+  ITEM_TYPE_PRODUCTS,
 };
