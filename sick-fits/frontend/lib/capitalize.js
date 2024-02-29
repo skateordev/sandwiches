9fef7bf7a281d908bff56a552bf239cb41a5b898
rob kerr <reallyrobkerr@gmail.com>
2024-02-29 02:46:28 -0800

    update capitalize jsdocs and early return value

--- sick-fits/frontend/lib/capitalize.js
+++ sick-fits/frontend/lib/capitalize.js
@@ -1,9 +1,11 @@
 /**
  * Takes the given string and makes the first character uppercase
- * @param string: the string to capitalize
+ * @param {string} string: the string to capitalize
  */
 
 function capitalize(string) {
+  if (!string) return null;
+
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
