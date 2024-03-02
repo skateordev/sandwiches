ea0d32b0f74847f3923a0c5b17fefd66c1bbc0f7
rob kerr <reallyrobkerr@gmail.com>
2024-03-01 16:16:01 -0800

    jest setup updates

--- sick-fits/frontend/jest.setup.js
+++ sick-fits/frontend/jest.setup.js
@@ -1,3 +1,6 @@
 import '@testing-library/jest-dom';
 
+/* any window.alert instances throughout the
+   code will be called as console.log instead.
+   this is required due to headless browser test running */
 window.alert = console.log;
