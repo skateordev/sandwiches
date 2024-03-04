712ad33c4dfa122a268efce32e612bef6ce571a0
rob kerr <reallyrobkerr@gmail.com>
2024-03-03 22:11:54 -0800

    successful test snapshot

---
+++ sick-fits/frontend/components/Products/__tests__/Product.test.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-undef */
+import { render } from '@testing-library/react';
+import { MockedProvider } from '@apollo/client/testing';
+import { fakeItem } from '../../../lib/testUtils';
+import Product from '../Product';
+import { CartStateProvider } from '../../../lib/cartState';
+
+const product = fakeItem();
+
+describe('<Product />', () => {
+  it('matches the default snapshot', () => {
+    const { container, debug } = render(
+      <CartStateProvider>
+        <MockedProvider>
+          <Product product={product} />
+        </MockedProvider>
+      </CartStateProvider>,
+    );
+
+    debug();
+    expect(container).toMatchSnapshot();
+  });
+});
